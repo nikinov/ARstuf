c3b2b994d39cb456ad2910a7e9c7223b6f6f0a71
nikinov <novellenicholas@gmail.com>
2019-12-24 19:50:40 +0100

    ...

---
+++ UnityARFoundationEssentials-b996c73a6300f2a1f75e79afbb48d1d1a7857b4d/Assets/My Stuf/MainMenu.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject Panel1;
+    public GameObject Panel2;
+
+    public void N()
+    {
+        Panel1.SetActive(false);
+        Panel2.SetActive(true);
+    }
+    public void P()
+    {
+        Panel1.SetActive(true);
+        Panel2.SetActive(false);
+    }
+
+    public void LoadScene1()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 1);
+	}
+	public void LoadScene2()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 2);
+	}
+	public void LoadScene3()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 3);
+	}
+	public void LoadScene4()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 4);
+	}
+	public void LoadScene5()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 5);
+	}
+	public void LoadScene6()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 6);
+	}
+	public void LoadScene7()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 7);
+	}
+	public void LoadScene8()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 8);
+	}
+	public void LoadScene9()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 9);
+	}
+	public void LoadScene10()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 10);
+	}
+	public void LoadScene11()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 11);
+	}
+	public void LoadScene12()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 12);
+	}
+	public void LoadScene13()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 13);
+	}
+	public void LoadScene14()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 14);
+	}
+	public void LoadScene15()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 15);
+	}
+	public void LoadScene16()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 16);
+	}
+	public void LoadScene17()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 17);
+	}
+	public void LoadScene18()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 18);
+	}
+	public void LoadScene19()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 19);
+	}
+	public void LoadScene20()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 20);
+	}
+	public void LoadScene21()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 21);
+	}
+	public void LoadScene22()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 22);
+	}
+	public void LoadScene23()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 23);
+	}
+	public void LoadScene24()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 24);
+	}
+	public void LoadScene25()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 25);
+	}
+	public void LoadScene26()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 26);
+	}
+	public void LoadScene27()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 27);
+	}
+	public void LoadScene28()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 28);
+	}
+	public void LoadScene29()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 29);
+	}
+	public void LoadScene30()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 30);
+	}
+	public void LoadScene31()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 31);
+	}
+	public void LoadScene32()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 32);
+	}
+	public void LoadScene33()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 33);
+	}
+	public void LoadScene34()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 34);
+	}
+	public void LoadScene35()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 35);
+	}
+	public void LoadScene36()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 36);
+	}
+	public void LoadScene37()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 37);
+	}
+	public void LoadScene38()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 38);
+	}
+	public void LoadScene39()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 39);
+	}
+	public void LoadScene40()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 40);
+	}
+	public void LoadScene41()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 41);
+	}
+	public void LoadScene42()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 42);
+	}
+	public void LoadScene43()
+	{
+		SceneManager.LoadScene(sceneBuildIndex: 43);
+	}
+}
